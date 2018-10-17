5059492c3f6d552ef10e4798e3bfbb63d51fa2fc
Ekaterina <egorkove@uci.edu>
2018-10-17 16:34:20 -0700

    All the relevant stuff here. need to figure out dependencies

--- GPSpi.py
+++ GPSpi.py
@@ -2,40 +2,28 @@
 
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+#### 500.2 mb B efi boot 103.8 fat32 efi system
 import pygame
 import thread
 import sys
 import time
 import datetime
 import csv
-import lib.nmea
-import lib.gps
-import lib.geomath
-import lib.alarm
-import lib.gui
+import nmea
+
+import gps.py
 
 
-class NAVSTAT():
 
-    def __init__(self):
+class GPSpi():
 
-        self.exit                = False
-        # Location and baudrate of serial device
-        self.serial_info         = [None ,None]
 
-        self.eng_tach_rose_1     = [[395 ,263 ,'0'] ,[273 ,152 ,'10'] ,[389 ,38 ,'20'] ,[506 ,152 ,'30']]
-        self.eng_tach_rose_2     = [[312 ,232 ,'5'] ,[298 ,75 ,'15'] ,[476 ,71 ,'25'] ,[475 ,232 ,'35']]
-
-        self.gps                 = lib.gps.GPS(self.gui, self.cache, self.unit)
-        # Get settings
-        self.settings()
 
     def start(self, csv_filename):
         '''Starts the NAVSTAT program and contains main loop.'''
         # Attempts to create a serial NMEA connection
         self.connect()
-        self.gps.track.distance_start()
+      #  self.gps.track.distance_start()
         # Main program loop - continue until quit
 
         # katya - "gps_data.csv" is the output filename. you can change it if you want
@@ -48,82 +36,12 @@ class NAVSTAT():
             log_file_writer.writerow([self.nmea.data_gps['utc'], self.nmea.data_gps['lat'], self.nmea.data_gps['lon'], self.nmea.data_gps['speed']])
             time.sleep(0.3) # set sampling rate in seconds. this takes a sample every 0.3 secs
 
-def settings(self):
-    '''Open navstat.config and load settings into respective variables'''
-    settings = open('navstat.config', 'r')
-    # Run through each line and load a setting
-    for line in settings:
-        # Line is not blank
-        if line != '\n' or '' or None:
-            # Line is not a comment
-            if line[0:1] != '#':
-                # Break up the setting name from contents
-                settings_item = line.split('=')
-                settings_item[1] = settings_item[1].rstrip()
-                # Load settings - more info in navstat.config
-                if settings_item[0] == 'frame_x':
-                    self.gui.size[1] = int(settings_item[1])
-                elif settings_item[0] == 'frame_y':
-                    self.gui.size[0] = int(settings_item[1])
-                elif settings_item[0] == 'top_speed':
-                    self.gps.speed_top = int(settings_item[1])
-                elif settings_item[0] == 'night_mode':
-                    if str(settings_item[1]) == 'OFF':
-                        self.gui.night = True
-                    else:
-                        self.gui.night = False
-                elif settings_item[0] == 'track_mode':
-                    if str(settings_item[1]) == 'OFF':
-                        self.gps.track.mode = True
-                    else:
-                        self.gps.track.mode = False
-                elif settings_item[0] == 'mini_mode':
-                    if str(settings_item[1]) == 'OFF':
-                        self.gui.mini = True
-                    else:
-                        self.gui.mini = False
-                elif settings_item[0] == 'track_secs':
-                    self.gps.track.save_info[0] = int(settings_item[1])
-                elif settings_item[0] == 'track_save':
-                    self.gps.track.save_info[1] = int(settings_item[1])
-                elif settings_item[0] == 'track_location':
-                    self.gps.track.location = str(settings_item[1])
-                elif settings_item[0] == 'track_maxsize':
-                    self.gps.track.maxsize = int(settings_item[1])
-                elif settings_item[0] == 'route_location':
-                    self.gps.route.location = str(settings_item[1])
-                elif settings_item[0] == 'unit_distance':
-                    if str(settings_item[1]) == 'KM':
-                        self.unit.measure[0] = 0
-                    elif str(settings_item[1]) == 'MI':
-                        self.unit.measure[0] = 1
-                    elif str(settings_item[1]) == 'NM':
-                        self.unit.measure[0] = 2
-                    self.unit.text[0] = settings_item[1]
-                elif settings_item[0] == 'unit_speed':
-                    if str(settings_item[1]) == 'KPH':
-                        self.unit.measure[1] = 0
-                    elif str(settings_item[1]) == 'MPH':
-                        self.unit.measure[1] = 1
-                    elif str(settings_item[1]) == 'NMPH':
-                        self.unit.measure[1] = 2
-                    self.unit.text[1] = settings_item[1].replace('PH' ,'')
-                elif settings_item[0] == 'gps_location':
-                    self.serial_info[0] = str(settings_item[1])
-                elif settings_item[0] == 'gps_baudrate':
-                    self.serial_info[1] = int(settings_item[1])
-                elif settings_item[0] == 'version':
-                    self.gui.version = settings_item[1]
-                elif settings_item[0] == 'xte_alarm':
-                    self.xte_alarm = settings_item[1]
-    settings.close()
-
 def connect(self):
     '''Determines whether a serial connection is available.'''
     x = 0
     connection = False
     # Readies a serial connection for NMEA GPS data
-    self.nmea = lib.nmea.NMEA0183(self.serial_info[0], self.serial_info[1], 5)
+    self.nmea = nmea.NMEA0183(self.serial_info[0], self.serial_info[1], 5)
     while connection == False:
         try:
             # Attempts to make a serial connection
@@ -146,154 +64,9 @@ def connect(self):
                 self.gps.route.mode = False
             x = x + 1
 
-def keyevents(self):
-    '''Checks whether a key has been pressed and activates event if so.'''
-    for event in pygame.event.get():
-        # Only KEYDOWN events
-        if event.type == pygame.KEYDOWN:
-            # Pressed 'Escape' to quit
-            if event.key == pygame.K_ESCAPE:
-                self.exit = True
-            # Pressed 'Tab' to mini mode
-            elif event.key == pygame.K_TAB:
-                self.gui.mini_mode()
-            # Pressed 'Space' to night mode
-            elif event.key == pygame.K_SPACE:
-                self.gui.night_mode()
-            # Pressed 'T' to tracking mode
-            elif event.key == pygame.K_t:
-                self.gps.track.switch()
-            # Pressed 'A' to autopilot kode
-            elif event.key == pygame.K_a:
-                self.auto_mode()
-            # Pressed 'Right' to move forward on route
-            elif event.key == pygame.K_RIGHT:
-                self.gps.route.get(0)
-            # Pressed 'Left' to move backward on route
-            elif event.key == pygame.K_LEFT:
-                self.gps.route.get(1)
-            elif event.key == pygame.K_F1:
-                self.navstat_mode = 0
-            elif event.key == pygame.K_F2:
-                self.navstat_mode = 1
-            elif event.key == pygame.K_F4:
-                self.navstat_mode = 3
-        elif event.type == pygame.QUIT:
-            self.quit()
-
-def error(self):
-    if self.navstat_mode == 0:
-        self.gui.txt_out((self.gui.font_2.render('There is currently no GPS connected.', True, self.gui.colour_2)) ,323
-                         ,472)
-        # Turns off track and route threads
-        if self.gps.track.mode == True:
-            self.gps.track.switch()
-        if self.gps.route.mode == True:
-            self.gps.route.switch()
-        # Clears the current cache
-        self.cache.gps = {'lat': 0, 'lon': 0, 'speed': 0, 'track': 0, 'utc': 0, 'status': ''}
-        # Reload settings
-        self.settings()
-        # Attempt to reconect to serial data
-        self.connect()
-        time.sleep(1)
 
 
-##############################################################
-##### ENG RELATED ############################################
-##############################################################
 
-def eng_interface(self):
-    '''Draws all the basic engine interface graphics.'''
-    # Fills the background color
-    self.ui_screen.fill(self.ui_colour_1)
-    # Draws the interface lines
-    pygame.draw.lines(self.ui_screen, self.ui_colour_2, False, [(250 ,0) ,(250 ,550)], 2)
-    pygame.draw.lines(self.ui_screen, self.ui_colour_2, False, [(550 ,0) ,(550 ,500)], 2)
-    pygame.draw.lines(self.ui_screen, self.ui_colour_2, False, [(0 ,465) ,(800 ,465)], 2)
-
-def eng_tachometer(self):
-    '''Positions and draws the tachometer interface.'''
-    rpm = 2100
-    # Draws t he basic compass interface
-    pygame.draw.rect(self.ui_screen, self.ui_colour_2, (250 ,0 ,300 ,20))
-    self.txt_out((self.ui_font_3.render('TAC', True, self.ui_colour_1)) ,380 ,0)
-    # Draws the tachometer rose
-    for point in self.eng_tach_rose_1:
-        self.txt_out(self.ui_font_2.render(point[2], True, self.ui_colour_2) ,point[0] ,point[1])
-    for point in self.eng_tach_rose_2:
-        self.txt_out(self.ui_font_2.render(point[2], True, self.ui_colour_2) ,point[0] ,point[1])
-    for point in self.gps_compass_rose_3:
-        pygame.draw.lines(self.ui_screen, self.ui_colour_2, False, point, 3)
-    # Determines tach circle position based on rpm
-    tach_rpm = (rpm *360 ) /4000
-    tach_rpm = (tach_rpm + 180) % 360
-    tach_main = self.calc_line(tach_rpm ,100 ,400 ,162)
-    ext = self.calc_size(rpm)
-    pygame.draw.lines(self.ui_screen, self.ui_colour_2, False, [(400 ,162) ,(tach_main[0] ,tach_main[1])], 5)
-    # Draws the compass interface
-    pygame.draw.circle(self.ui_screen, self.ui_colour_2, (400 ,162), 5)
-    pygame.draw.circle(self.ui_screen, self.ui_colour_2, (400 ,162), 100 ,1)
-    self.txt_out((self.ui_font_4.render(str(round(rpm)).replace('.0' ,''), True, self.ui_colour_2)) ,348 + ext ,290)
-
-##############################################################
-##### AIS RELATED ############################################
-##############################################################
-
-def aismap(self ,compass_out):
-    self.ui_screen.fill(self.ui_colour_1)
-    self.aismap_data = [[121334543 ,44.54204 ,-80.03334, 175.0] ,[12123232 ,44.50679 ,-79.84108, 228.0]
-                        ,[12123232 ,44.42924 ,-79.97292, 115.0] ,[12123232 ,44.50471 ,-80.19505, 10.0]]
-    for vessel in self.aismap_data:
-        vessel_data = self.haversine(self.nmea_connection.lat ,self.nmea_connection.lon ,vessel[1] ,vessel[2])
-        if vessel_data[0] < 20:
-            vessel_distance = round(vessel_data[0 ] *10 ,1)
-            vessel_position = self.calc_line(vessel_data[1] ,vessel_distance ,400 ,225)
-            vessel_cog = self.calc_line(vessel[3] ,12 ,vessel_position[0] ,vessel_position[1])
-            pygame.draw.circle(self.ui_screen, self.ui_colour_2, (vessel_position[0] ,vessel_position[1]), 2)
-            pygame.draw.circle(self.ui_screen, self.ui_colour_2, (vessel_position[0] ,vessel_position[1]), 12, 1)
-            pygame.draw.lines(self.ui_screen, self.ui_colour_2, False, [(vessel_position[0] ,vessel_position[1]) ,(vessel_cog[0] ,vessel_cog[1])], 1)
-    compass_main = self.calc_line(compass_out ,200 ,400 ,225)
-    pygame.draw.circle(self.ui_screen, self.ui_colour_2, (400 ,225), 200 ,1)
-    pygame.draw.lines(self.ui_screen, self.ui_colour_2, False, [(400 ,225) ,(compass_main[0] ,compass_main[1])], 3)
-
-def ais_start(self):
-    while self.ais == True:
-        return
-
-def auto_start(self):
-    while self.auto == True:
-        return
-
-def auto_mode(self):
-    '''Checks whether Auto Mode is enabled, and turns it on if so.'''
-    if self.auto == False:
-        thread.start_new_thread(self.auto_start, ())
-        self.auto = True
-    else:
-        self.auto = False
-
-def error_out(self ,error_text, x, y):
-    '''Creates an error splash screen to output error detail.
-
-    Keyword arguments:
-    error_text -- the error number and text details
-    x -- the x position of the text
-    y -- the y position of the text
-
-    '''
-    while True:
-        self.ui_screen.fill(self.ui_colour_1)
-        self.txt_out((self.ui_font_2.render('Error', True, self.ui_colour_2)) ,375 ,230)
-        self.txt_out((self.ui_font_3.render(error_text, True, self.ui_colour_2)) ,x ,y)
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                # Pressed 'Escape' to quit
-                if event.key == pygame.K_ESCAPE:
-                    self.quit()
-            if event.type == pygame.QUIT:
-                self.quit()
 
 def quit(self):
     '''Gets NAVSTAT ready to quit.'''
@@ -314,7 +87,7 @@ def quit(self):
 
 # import cProfile
 if __name__ == "__main__":
-    gps = NAVSTAT()
+    gps = GPSpi()
     # set filename of your csv output file
     csv_filename = "gps_data.csv"
     gps.start(csv_filename)
---
+++ nmea.py
@@ -0,0 +1,180 @@
+import re
+import serial
+import binascii
+
+from threading import Thread
+
+
+class NMEA0183():
+
+    def __init__(self, location, baud_rate, timeout):
+        '''Initiates variables and opens serial connection.
+
+        Keyword arguments:
+        location -- the location of the serial connection
+        baud_rate -- the baud rate of the connection
+        timeout -- the timeout of the connection
+        type -- the type of connection.'GPS'
+
+        '''
+        self.exit = False
+        self.location = location
+        self.baud_rate = baud_rate
+        self.timeout = timeout
+        self.serial_dev = None
+        self.serial_data = None
+        self.ais_data = None
+
+        # Ready the GPS variables
+        self.data_gps = {'lat': float(0.0), 'lon': float(0.0), 'speed': float(0.0), 'track': float(0.0), 'utc': '0.0',
+                         'status': 'A'}
+
+    def read(self):
+        '''Creates a thread to read serial connection data.'''
+        self.serial_dev = serial.Serial(self.location, self.baud_rate, self.timeout)
+        serial_thread = Thread(None, self.read_thread, None, ())
+        serial_thread.start()
+
+    def send(self):
+        return
+
+    def read_thread(self):
+        '''The thread used to read incoming serial data.'''
+        dat_new = ''
+        dat_old = ''
+        # Loops until the connection is broken, or is instructed to quit
+        try:
+            while self.is_open():
+                # Instructed to quit
+                if self.exit:
+                    break
+                if dat_new:
+                    dat_old = dat_new
+                    dat_new = ''
+                dat_old = dat_old + self.buffer()
+                if re.search("\r\n", dat_old):
+                    try:
+                        self.serial_data, dat_new = dat_old.split("\r\n")
+                    except:
+                        pass
+                    if self.checksum(self.serial_data):
+                        # Incoming serial data is GPS related
+                        if self.serial_data[0:3] == '$GP':
+                            self.gps()
+
+        except:
+            self.quit()
+
+    def is_open(self):
+        '''Checks whether the serial connection is still open.'''
+        return self.serial_dev.isOpen()
+
+    def buffer(self):
+        '''Creates a buffer for serial data reading. Avoids reading lines for better performance.'''
+        dat_cur = self.serial_dev.read(1)
+        x = self.serial_dev.inWaiting()
+        if x: dat_cur = dat_cur + self.serial_dev.read(x)
+        return dat_cur
+
+    def makechecksum(self, data):
+        '''Calculates a checksum from a NMEA sentence.
+
+        Keyword arguments:
+        data -- the NMEA sentence to create
+
+        '''
+        csum = 0
+        i = 0
+        # Remove ! or $ and *xx in the sentence
+        data = data[1:data.rfind('*')]
+        while (i < len(data)):
+            input = binascii.b2a_hex(data[i])
+            input = int(input, 16)
+            # xor
+            csum = csum ^ input
+            i += 1
+        return csum
+
+    def checksum(self, data):
+        '''Initiates variables and opens serial connection.
+
+        Keyword arguments:
+        data -- the NMEA sentence to check
+
+        '''
+        try:
+            # Create an integer of the two characters after the *, to the right
+            supplied_csum = int(data[data.rfind('*') + 1:data.rfind('*') + 3], 16)
+        except:
+            return ''
+        # Create the checksum
+        csum = self.makechecksum(data)
+        # Compare and return
+        if csum == supplied_csum:
+            return True
+        else:
+            return False
+
+    def gps(self):
+        '''Deconstructs NMEA gps readings.'''
+        if self.serial_data[3:6] == 'RMC':
+            self.serial_data = self.serial_data.split(',')
+            self.data_gps['utc'] = self.gps_nmea2utc()
+            self.data_gps['status'] = self.serial_data[2]
+            self.data_gps['lat'] = self.gps_nmea2dec(0)
+            self.data_gps['lon'] = self.gps_nmea2dec(1)
+            self.data_gps['speed'] = float(self.serial_data[7])
+            self.data_gps['track'] = float(self.serial_data[8])
+
+    def gps_nmea2dec(self, type):
+        '''Converts NMEA lat/long format to decimal format.
+
+        Keyword arguments:
+        type -- tells whether it is a lat or long. 0=lat,1=long
+
+        '''
+        # Represents the difference in list position between lat/long
+        x = type * 2
+        # Converts NMEA format to decimal format
+        data = float(self.serial_data[3 + x][0:2 + type]) + float(self.serial_data[3 + x][2 + type:9 + type]) / 60
+        # Adds negative value based on N/S, W/E
+        if self.serial_data[4 + x] == 'S':
+            data = data * (-1)
+        elif self.serial_data[4 + x] == 'W':
+            data = data * (-1)
+        return data
+
+    def gps_nmea2utc(self):
+        '''Converts NMEA utc format to more standardized format.'''
+        time = self.serial_data[1][0:2] + ':' + self.serial_data[1][2:4] + ':' + self.serial_data[1][4:6]
+        date = '20' + self.serial_data[9][4:6] + '-' + self.serial_data[9][2:4] + '-' + self.serial_data[9][0:2]
+        return date + 'T' + time + 'Z'
+
+    def quit(self):
+        '''Enables quiting the serial connection.'''
+        self.exit = True
+
+
+class CACHE():
+
+    def __init__(self):
+        self.gps = {'lat': 0, 'lon': 0, 'speed': 0, 'track': 0, 'utc': 0, 'status': ''}
+
+    def cache_gps(self, lat, lon, speed, track, utc, status):
+        '''Used to store a common gps data sequence for future access.
+
+        Keyword arguments:
+        lat - the current latitude
+        lon - the current longitude
+        speed - the current speed
+        track - the current track
+        utc - the current utc
+        status - the current gps status
+
+        '''
+        self.gps['lat'] = lat
+        self.gps['lon'] = lon
+        self.gps['speed'] = speed
+        self.gps['track'] = track
+        self.gps['utc'] = utc
+        self.gps['status'] = status
